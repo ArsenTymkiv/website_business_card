ae09d5772dad5a007ba50181eb1970ce1e1d1db8
Gvars <arseny.tymkiv.ua@gmail.com>
2022-09-09 18:32:13 +0300

    add scroll menu edit responsive

--- scripts/mine.js
+++ scripts/mine.js
@@ -2,5 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     let button = document.querySelector('#icon');
     let menu = document.querySelector('.hamburger_links');
     // console.log(button)
-    button.addEventListener('click', () => menu.classList.toggle('nav_active'))
+    button.addEventListener('click', () => menu.classList.toggle('nav_active'));
+
+    let header = document.querySelector('header');
+
   });
+
+  window.onscroll = function() {showHeader()};
+
+function showHeader() {
+  let header = document.querySelector('header');
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+}
