682d926a03fa180b3b115a5b3646631548f6df12
Gvars <arseny.tymkiv.ua@gmail.com>
2022-09-12 15:23:58 +0300

    fix mobile menu

--- scripts/mine.js
+++ scripts/mine.js
@@ -1,63 +1,73 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-  /*  BUTTON MOBILE */
-  let button = document.querySelector('#icon');
-  let menu = document.querySelector('.hamburger_links');
-  button.addEventListener('click', () =>{ 
-    
-    menu.classList.toggle('nav_active');
-    button.childNodes[1].classList.toggle("fa-xmark");
+  /*  MOBILE MENU */
+
+  let mobile_menu_button = document.querySelector('.mobile_nav');
+  let mobile_menu_items = document.querySelector('.mobile_menu');
+
+  mobile_menu_button.addEventListener('click', () => {
+    mobile_menu_items.classList.toggle('nav_active');
+    handleMobileButton();
   });
 
-
-  
-
- /* SCROLL ACTIVE MENU*/
-  let arr_sel = document.querySelectorAll(`[href*="section"]`);
-  let elements1 = document.querySelectorAll(`[id^="section"]`);
- 
- 
-  window.addEventListener('scroll', function() {
-    
-    elements1.forEach((element) =>{
-    var position = element.getBoundingClientRect();
-    // checking for partial visibility
-    if(position.top < window.innerHeight && position.bottom >= 50) {
-      arr_sel.forEach((a_el)=>{
-        let r = a_el.href.match(/[^#]+$/);
-        console.log(r[0]);
-        if(r[0]===element.id){
-          a_el.classList.add("active");
-        }else if(element.id==="section7"){
-          a_el.classList.remove("active");
-          document.querySelector(".cta button").classList.add("active_b");
-        }else{
-          a_el.classList.remove("active");
-          document.querySelector(".cta button").classList.remove("active_b");
-        };
-       
-      })
+  document.body.addEventListener('click', (e) => {
+    console.log(e.target);
+    console.log(e.target !== document.querySelector('.mobile_nav'));
+    if (mobile_menu_items.classList.contains('nav_active') && e.target !== document.querySelector(".mobile_nav i")) {
+      handleMobileButton(true);
     }
-  })
-
-
-
   });
+
+  window.addEventListener('scroll', function () {
+    onScrollEventHandler();
+  });
+
+
+
+  function handleMobileButton(isBody = false) {
+    let button_icon = document.querySelector(".mobile_nav i");
+    if (isBody) mobile_menu_items.classList.toggle('nav_active');
+      button_icon.classList.toggle("fa-bars");
+      button_icon.classList.toggle("fa-xmark");
+  }
+
 
 
 
 
 });
+
+
+
+
+
+
 /* SCROLL HEADER  fixed*/
-window.onscroll = function() {showHeader()};
+window.onscroll = function () { showHeader() };
 
 function showHeader() {
-let header = document.querySelector('header');
-if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-  header.classList.add('fixed');
-} else {
-  header.classList.remove('fixed');
+  let header = document.querySelector('header');
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
 }
+
+/* SCROLL ACTIVE MENU*/
+function onScrollEventHandler() {
+  // let arr_sel = document.querySelectorAll(`[href*="section"]`);
+  let arrSections = document.querySelectorAll('[id^="section"]');
+  arrSections.forEach((element) => {
+    var position = element.getBoundingClientRect();
+    if (position.top > 0 && position.top < window.innerHeight && position.bottom >= 0) {
+
+      if (document.querySelector('.active')) document.querySelector('.active').classList.remove('active');
+      if (document.querySelector('a[href="#' + element.id + '"]')) {
+        document.querySelector('a[href="#' + element.id + '"]').classList.add('active');
+      }
+    }
+  })
 }
 
 
